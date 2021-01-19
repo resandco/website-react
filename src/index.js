5f304edaa81585d7932862cf9b61e00120137c77
Justine <hell.justine@gmail.com>
2021-01-19 17:23:01 +0100

    ajout composant ScrollToTop pour gérer le scroll

--- src/index.js
+++ src/index.js
@@ -4,9 +4,11 @@ import './index.css';
 import App from './App';
 import { BrowserRouter as Router } from 'react-router-dom'
 import * as serviceWorker from './serviceWorker';
+import ScrollToTop from './components/common/ScrollToTop';
 
 ReactDOM.render(
     <Router>
+        <ScrollToTop />
         <App />
     </Router>,
   document.getElementById('root')
