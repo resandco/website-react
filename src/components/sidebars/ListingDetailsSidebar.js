d05dbca15e32b4c829596767ce308c721952bc43
Louis-Rémi Babé <lrbabe@gmail.com>
2021-06-08 15:15:19 +0200

    Jotform integration ready

--- src/components/sidebars/ListingDetailsSidebar.js
+++ src/components/sidebars/ListingDetailsSidebar.js
@@ -1,10 +1,13 @@
 import React, { useState } from 'react'
-import { addDays, getDay, format } from 'date-fns'
+import { addDays, getDay, format, isToday, getHours } from 'date-fns'
 import { fr } from 'date-fns/locale'
 import Select from 'react-select'
 import DatePicker from 'react-datepicker'
 
 import 'react-datepicker/dist/react-datepicker.css'
+
+const WEBHOOK_URL =
+    'https://hook.integromat.com/ky8kxgjfcj12okupxur9y6bqnvc53rfn'
 
 const dayNameToNumber = (name) =>
     // This will be matched with dateFns.getDay which returns 0 for Sunday
@@ -27,9 +30,25 @@ function numberToEuroPrice(number = 0) {
         : `${number}€`
 }
 
+const modesConso = [
+    {
+        label: 'Sur place (+0€)',
+        value: 'eatIn',
+    },
+    {
+        label: 'À emporter (+0€)',
+        value: 'takeAway',
+    },
+    {
+        label: 'Livraison dans un rayon de 3kms (+4€)',
+        value: 'delivery',
+    },
+]
+
 function ListingDetailsSidebar({ restaurant }) {
     const [selectedDate, setDate] = useState()
     const [selectedTime, setTime] = useState()
+    const [selectedConso, setConso] = useState()
     const [isTimepickerDisabled, setTimepickerDisabled] = useState(true)
     const [timepickerOptions, setTimepickerOptions] = useState([])
     const [menus, setMenus] = useState(restaurant.menus.map(() => 0))
@@ -45,12 +64,20 @@ function ListingDetailsSidebar({ restaurant }) {
         setTimepickerDisabled(false)
         setTime(null)
 
+        const currentHours = getHours(new Date())
         const timeslots = Object.keys(restaurant.creneaux)
             .filter(
                 (key) => dayNameToNumber(key.split(' ')[0]) === getDay(date)
             )
             .map((key) => restaurant.creneaux[key])
             .flat()
+            .filter(
+                (time) =>
+                    !isToday(date) ||
+                    currentHours +
+                        (selectedConso?.value === 'delivery' ? 3 : 2) <=
+                        Number(time.replace(/h\d*/, ''))
+            )
             .map((time) => ({
                 value: time.replace('h', ':').padEnd(5, '0'),
                 label: time,
@@ -74,52 +101,64 @@ function ListingDetailsSidebar({ restaurant }) {
         menus.reduce(
             (sum, nbMenu, idMenu) =>
                 sum + restaurant.menus[idMenu].prix * nbMenu,
-            0
+            selectedConso?.value === 'delivery' ? 4 : 0
         )
     const isSubmitDisabled = () =>
         selectedDate == null ||
         selectedTime == null ||
         isCgvChecked === false ||
         getTotal() === 0
+    const getQueryString = (dateFormat = 'yyyy-MM-dd') => {
+        return [
+            `restoId=${restaurant.id}`,
+            `restoName=${restaurant.title}`,
+            `resaDate=${encodeURIComponent(format(selectedDate, dateFormat))}`,
+            `resaTime=${selectedTime.label}`,
+            `consumingMode=${selectedConso.value}`,
+            `isCgvChecked=${isCgvChecked}`,
+            `total=${getTotal()}`,
+            `order=${encodeURIComponent(
+                menus
+                    .filter((nbMenu) => nbMenu > 0)
+                    .map(
+                        (nbMenu, idMenu) =>
+                            `${restaurant.menus[idMenu].menu} x ${nbMenu}`
+                    )
+                    .join('\n\n')
+            )}`,
+            `isTest=${process.env.NODE_ENV !== 'production'}`,
+        ].join('&')
+    }
     const getFormUrl = () =>
         !isSubmitDisabled()
-            ? [
-                  'https://tripetto.app/run/E7I87U7J8X',
-                  `?placeId=${restaurant.id}`,
-                  `&placeName=${restaurant.title}`,
-                  `&date=${encodeURIComponent(
-                      format(selectedDate, 'dd/MM/yyyy')
-                  )}`,
-                  `&time=${selectedTime.label}`,
-                  ...menus.map((nbMenu, idMenu) =>
-                      nbMenu
-                          ? `&menu${idMenu}=${encodeURIComponent(
-                                restaurant.menus[idMenu].menu
-                            )}%20x${nbMenu}`
-                          : ''
-                  ),
-                  `&displayedTotal=${getTotal()}`,
-                  `&isCgvChecked=${isCgvChecked}`,
-              ].join('')
+            ? `${WEBHOOK_URL}?${getQueryString('dd/MM/yyyy')}`
             : '#nogo'
     const getTarget = () => (!isSubmitDisabled() ? '_blank' : '')
 
     return (
         <>
             <div className="sidebar section-bg">
-                <div className="contact-form-action">
-                    <div className="form-group">
-                        <DatePicker
-                            selected={selectedDate}
-                            placeholderText="Date de réservation"
-                            onChange={handleDateChange}
-                            startDate={today}
-                            endDate={inTwoMonths}
-                            filterDate={isOpen}
-                            locale={fr}
-                            dateFormat="dd/MM/yyyy"
-                        />
-                    </div>
+                <div className="mode padding-bottom-20px">
+                    <Select
+                        className="select-wrapper"
+                        value={selectedConso}
+                        onChange={setConso}
+                        placeholder="Mode de consommation"
+                        options={modesConso}
+                    />
+                </div>
+
+                <div className="contact-form-action padding-bottom-20px">
+                    <DatePicker
+                        selected={selectedDate}
+                        placeholderText="Date de réservation"
+                        onChange={handleDateChange}
+                        startDate={today}
+                        endDate={inTwoMonths}
+                        filterDate={isOpen}
+                        locale={fr}
+                        dateFormat="dd/MM/yyyy"
+                    />
                 </div>
 
                 <div className="time-slots padding-bottom-30px">
@@ -137,12 +176,15 @@ function ListingDetailsSidebar({ restaurant }) {
                     <ul>
                         {restaurant.menus.map((menu, key) => {
                             return (
-                                <li className="card-item" key={key}>
+                                <li
+                                    className="card-item padding-bottom-20px"
+                                    key={key}
+                                >
                                     <div className="card-title">
                                         {menu.menu}
                                     </div>
                                     <div className="card-sub d-flex justify-content-between">
-                                        <span className="color-text">
+                                        <span className="color-text padding-top-5px">
                                             {numberToEuroPrice(menu.prix)}
                                         </span>
                                         <span className="">
@@ -152,6 +194,7 @@ function ListingDetailsSidebar({ restaurant }) {
                                                 data-key={key}
                                                 value={menus[key]}
                                                 onChange={handleMenuChange}
+                                                min="0"
                                             />
                                         </span>
                                     </div>
@@ -194,7 +237,7 @@ function ListingDetailsSidebar({ restaurant }) {
                 <div className="card-item">
                     <div className="card-title">Total</div>
                     <div className="card-sub d-flex justify-content-between">
-                        <span className="">Règlement au restaurant</span>
+                        <span className="">Règlement en ligne</span>
                         <span className="color-text">
                             {numberToEuroPrice(getTotal())}
                         </span>
